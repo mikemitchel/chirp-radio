60e97f66264d4d298c8b9af7ebd53b6ed725995f
mitch <mitch@nomadiklabs.com>
2025-09-30 17:32:08 -0500

    cleanup main.ts, remove sample header/footer

--- src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/Footer.tsx
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center text-sm p-2">
-      © 2025 My Web Player
-    </footer>
-  );
-}
--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/Header.tsx
-export default function Header() {
-  return (
-    <header className="bg-gray-800 text-white p-4 text-center">
-      <h1>🎵 My Web Player</h1>
-    </header>
-  );
-}
