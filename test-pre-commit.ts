1e0c9cc2fe14a1ffc9a22c5d71d91086295a3c2e
mitch <mitch@nomadiklabs.com>
2025-10-14 13:48:02 -0400

    Test pre-commit hook

---
+++ test-pre-commit.ts
@@ -0,0 +1,8 @@
+// Test file with formatting issues
+const foo = 'bar' // Extra spaces and semicolon
+const baz = { a: 1, b: 2 } // No spaces in object
+
+export const testFunction = () => {
+  console.log(foo, baz)
+  return true
+}
