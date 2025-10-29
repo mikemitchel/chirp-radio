139936a8d6b2c568bb6a46441fa55ba1df5e2820
Ryan <design@ryangwilson.com>
2025-10-28 20:02:09 -0400

    updates the claude instructions doc

--- src/pages/AndroidAutoPage.tsx
+++ src/pages/AndroidAutoPage.tsx
@@ -1,5 +1,13 @@
 import CrStreamingMusicPlayer from '../stories/CrStreamingMusicPlayer'
+import { AudioPlayerProvider } from '../contexts/AudioPlayerContext'
 
 export const AndroidAutoPage = () => {
-  return <CrStreamingMusicPlayer variant="android-auto" />
+  return (
+    <AudioPlayerProvider
+      autoFetch={true}
+      apiUrl="https://chirpradio.appspot.com/api/current_playlist"
+    >
+      <CrStreamingMusicPlayer variant="android-auto" autoFetch={true} />
+    </AudioPlayerProvider>
+  )
 }
